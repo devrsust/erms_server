1f5d70d3939f92d20e521702947c70226b2c11a1
Hanniel.Daniel <dhanniel21@gmail.com>
2026-04-22 12:42:32 +0100

    Deployment

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,21 +1,46 @@
+// import { Injectable } from '@nestjs/common';
+// import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+// import { ConfigService } from '@nestjs/config';
+// import { PrismaClient } from 'generated/prisma/client';
+
+
+// @Injectable()
+// export class PrismaService extends PrismaClient {
+
+//     constructor(configService: ConfigService) {
+//         const url = configService.get("DATABASE_URL");
+
+//         if (!url) {
+//             throw new Error('DATABASE_URL is not defined');
+//         }
+
+//         super({
+//             adapter: new PrismaMariaDb(url),
+//         });
+//     }
+// }
+
 import { Injectable } from '@nestjs/common';
-import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from 'generated/prisma/client';
-
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
+  constructor(configService: ConfigService) {
+    const url = configService.get<string>('DATABASE_URL');
 
-    constructor(configService: ConfigService) {
-        const url = configService.get("DATABASE_URL");
-
-        if (!url) {
-            throw new Error('DATABASE_URL is not defined');
-        }
-
-        super({
-            adapter: new PrismaMariaDb(url),
-        });
+    if (!url) {
+      throw new Error('DATABASE_URL is not defined');
     }
+
+    const pool = new Pool({
+      connectionString: url,
+    });
+
+    super({
+      adapter: new PrismaPg(pool),
+    });
+  }
 }
