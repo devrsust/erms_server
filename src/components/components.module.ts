50aaf80863d9d080db2ab36e9a581a0481e9612b
Hanniel.Daniel <dhanniel21@gmail.com>
2026-04-22 12:27:37 +0100

    Deployment

--- src/components/components.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ComponentsService } from './components.service';
-import { ComponentsController } from './components.controller';
-
-@Module({
-  controllers: [ComponentsController],
-  providers: [ComponentsService],
-})
-export class ComponentsModule {}
